4e64fe9bd1e11e80901511f39a3c2f3ccd448735
MateoT5 <matute25155@gmail.com>
2026-01-21 12:46:47 -0300

    Primera Week

---
+++ 0 Mini Ejecicio/0 Mini Ejecicio/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Mini_Ejecicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO
+            // 21/01/26 Creado por Mateo Ternovic
+
+            /*
+            const float Pi = 3.1415926535f;
+
+            float rad;
+
+            Console.WriteLine("Ingrese el radio de la esfera");
+            rad = float.Parse(Console.ReadLine());
+
+            float superficie = 4 * Pi * (rad * rad);
+            float volumen = (4/3) * Pi * (rad * rad * rad);
+
+            Console.WriteLine($"La superficie de la esfera es: {superficie}");
+            Console.WriteLine($"El volumen de la esfera es: {volumen}");
+            */
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            
+            float num1;
+            float num2;
+
+            Console.WriteLine("Ingrese un numero: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un segundo numero: ");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Suma: {num1+num2}");
+            Console.WriteLine($"Resta: {num1-num2}");
+            Console.WriteLine($"Multiplicación: {num1*num2}");
+            Console.WriteLine($"División: {num1/num2}");
+            Console.WriteLine($"Resto: {num1%num2}");
+            
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////
+            
+
+
+            A
+
+        }
+    }
+}
